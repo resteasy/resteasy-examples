ada68cad021bc0539b82260b13a6ecb7d2ba2ed8
阿男 <l.weinan@gmail.com>
2019-09-25 10:05:25 +0800

    RESTEASY-2349 reenable-jaxrs-2.0.workbook (#27)
    * cleanup oreilly-workbook pom
    
    * enable ex04_1
    
    * - enable ex04_2
    - cleanup jaxrs book pom
    
    * enable ex04_3
    
    * ex05_1
    
    * enable ex05_02
    
    * enable ex06_1
    
    * ex06_2
    
    * enable ex07_1
    
    * enable ex09_1
    
    * enable ex09_2
    
    * enable ex10_1
    enable ex10_2
    enable ex11_1
    enable ex12_1
    fix + enable ex13_1
    
    * enable ex15_1
    
    * delete eclipse project files
    
    * enable ex12_2 / ex13_1
    
    * enable ex15_1
    
    * replace outdated Base64 class

--- oreilly-jaxrs-2.0-workbook/ex12_2/src/test/java/com/restfully/shop/test/CustomerResourceTest.java
+++ oreilly-jaxrs-2.0-workbook/ex12_2/src/test/java/com/restfully/shop/test/CustomerResourceTest.java
@@ -17,45 +17,41 @@ import javax.ws.rs.core.Response;
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
  */
-public class CustomerResourceTest
-{
-   private static Client client;
+public class CustomerResourceTest {
+    private static Client client;
 
-   @BeforeClass
-   public static void initClient()
-   {
-      client = ClientBuilder.newClient();
-   }
+    @BeforeClass
+    public static void initClient() {
+        client = ClientBuilder.newClient();
+    }
 
-   @AfterClass
-   public static void closeClient()
-   {
-      client.close();
-   }
+    @AfterClass
+    public static void closeClient() {
+        client.close();
+    }
 
-   @Test
-   public void testCustomerResource() throws Exception
-   {
-      System.out.println("*** Create a new Customer ***");
-      Customer newCustomer = new Customer();
-      newCustomer.setFirstName("Bill");
-      newCustomer.setLastName("Burke");
-      newCustomer.setStreet("256 Clarendon Street");
-      newCustomer.setCity("Boston");
-      newCustomer.setState("MA");
-      newCustomer.setZip("02115");
-      newCustomer.setCountry("USA");
+    @Test
+    public void testCustomerResource() throws Exception {
+        System.out.println("*** Create a new Customer ***");
+        Customer newCustomer = new Customer();
+        newCustomer.setFirstName("Bill");
+        newCustomer.setLastName("Burke");
+        newCustomer.setStreet("256 Clarendon Street");
+        newCustomer.setCity("Boston");
+        newCustomer.setState("MA");
+        newCustomer.setZip("02115");
+        newCustomer.setCountry("USA");
 
-      Response response = client.target("http://localhost:8080/services/customers")
-              .request().post(Entity.xml(newCustomer));
-      if (response.getStatus() != 201) throw new RuntimeException("Failed to create");
-      String location = response.getLocation().toString();
-      System.out.println("Location: " + location);
-      response.close();
+        Response response = client.target("http://localhost:8080/services/customers")
+                .request().post(Entity.xml(newCustomer));
+        if (response.getStatus() != 201) throw new RuntimeException("Failed to create");
+        String location = response.getLocation().toString();
+        System.out.println("Location: " + location);
+        response.close();
 
-      System.out.println("*** GET Created Customer **");
-      response = client.target(location).request().get();
-      String md5 = response.getHeaderString("Content-MD5");
-      System.out.println("Content-MD5: " + md5);
-   }
+        System.out.println("*** GET Created Customer **");
+        response = client.target(location).request().get();
+        String md5 = response.getHeaderString("Content-MD5");
+        System.out.println("Content-MD5: " + md5);
+    }
 }
