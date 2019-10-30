090a397bf483ad7af328bc101a3bbcac7f868304
阿男 <l.weinan@gmail.com>
2019-10-31 00:30:25 +0800

    fix springMVC test (#30)

--- resteasy-springMVC/src/main/java/org/jboss/resteasy/examples/springmvc/ContactService.java
+++ resteasy-springMVC/src/main/java/org/jboss/resteasy/examples/springmvc/ContactService.java
@@ -10,29 +10,25 @@ import java.util.concurrent.ConcurrentHashMap;
  * Controller/Service/Repository layering is a pretty common design pattern in
  * Spring projects. While this example doesn't have a Repository/DAO, the
  * ContactService class will show how to integrate Controllers with the layers
- * below in a Spring/RESTEasy application. 
- * 
+ * below in a Spring/RESTEasy application.
+ *
  * @author <a href="mailto:sduskis@gmail.com">Solomon Duskis</a>
  * @version $Revision: 1 $
  */
 
 @Service
-public class ContactService
-{
+public class ContactService {
    private Map<String, Contact> contactMap = new ConcurrentHashMap<String, Contact>();
 
-   public void save(Contact contact)
-   {
+   public void save(Contact contact) {
       contactMap.put(contact.getLastName(), contact);
    }
 
-   public Contact getContact(String lastName)
-   {
+   public Contact getContact(String lastName) {
       return contactMap.get(lastName);
    }
 
-   public Contacts getAll()
-   {
+   public Contacts getAll() {
       return new Contacts(contactMap.values());
    }
 }
