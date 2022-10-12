7c20669dcdf78c8fdd1c062689ad71d93233af14
James R. Perkins <jperkins@redhat.com>
2022-10-12 14:15:15 -0700

    Migrate the Spring examples to resteasy-spring-examples https://github.com/resteasy/resteasy-spring-examples/pull/1.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- resteasy-spring-rest/src/main/java/org/jboss/resteasy/examples/springrest/MyApplication.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.resteasy.examples.springrest;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class MyApplication extends Application {
-
-}
