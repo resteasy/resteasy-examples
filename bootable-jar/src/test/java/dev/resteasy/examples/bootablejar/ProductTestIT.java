86fd4d5bfeb303822abd7f9c2c63189498f18100
James R. Perkins <jperkins@redhat.com>
2024-12-18 10:05:57 -0800

    Allow Java 11 to work by defaulting to WIldFly 34. Also do some cleanup of the POM's and some tests.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- bootable-jar/src/test/java/dev/resteasy/examples/bootablejar/ProductTestIT.java
+++ bootable-jar/src/test/java/dev/resteasy/examples/bootablejar/ProductTestIT.java
@@ -5,10 +5,27 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Response;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(ArquillianExtension.class)
+@RunAsClient
 public class ProductTestIT {
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        // Dummy deployment
+        return ShrinkWrap.create(WebArchive.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
     @Test
     public void testResponse() {
         try (Client client = ClientBuilder.newClient()) {
--- examples-jsapi/src/test/java/dev/resteasy/examples/OrdersTest.java
+++ examples-jsapi/src/test/java/dev/resteasy/examples/OrdersTest.java
@@ -1,48 +1,30 @@
 package dev.resteasy.examples;
 
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.SeBootstrap;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.Response;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import dev.resteasy.junit.extension.annotations.RequestPath;
+import dev.resteasy.junit.extension.annotations.RestBootstrap;
 
 /**
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
  */
+@RestBootstrap(OrdersTest.TestApplication.class)
 public class OrdersTest {
 
-    private static SeBootstrap.Instance SERVER;
-
-    @BeforeAll
-    public static void startServer() throws Exception {
-        SERVER = SeBootstrap.start(TestApplication.class).toCompletableFuture().get(5, TimeUnit.SECONDS);
-    }
-
-    @AfterAll
-    public static void stopServer() throws Exception {
-        if (SERVER != null) {
-            SERVER.stop().toCompletableFuture().get(5, TimeUnit.SECONDS);
-        }
-    }
-
     @Test
-    public void createOrder() {
-        try (Client client = ClientBuilder.newClient()) {
-            final Response response = client.target(SERVER.configuration().baseUriBuilder().path("/orders/1"))
-                    .request().get();
-            Assertions.assertEquals(Response.Status.OK, response.getStatusInfo(), () -> response.readEntity(String.class));
-            final Order order = response.readEntity(Order.class);
-            Assertions.assertEquals(1, order.getId());
-        }
+    public void createOrder(@RequestPath("/orders/1") final WebTarget target) {
+        final Response response = target.request().get();
+        Assertions.assertEquals(Response.Status.OK, response.getStatusInfo(), () -> response.readEntity(String.class));
+        final Order order = response.readEntity(Order.class);
+        Assertions.assertEquals(1, order.getId());
     }
 
     @ApplicationPath("/")
